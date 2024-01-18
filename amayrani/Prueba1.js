8cd42e6184d4d2746470a974ae46dd0552eb4adc
amayraniarg <amayrani.armas@grupoxalka.com>
2024-01-18 10:38:42 -0600

    Modifico js de file amayran

---
+++ amayrani/Prueba1.js
@@ -0,0 +1,35 @@
+//prueba1 BMI 
+//BMI = masa /altura /** 2 = masa / (altura * altura)
+
+let markHigherBMI = true; 
+
+let masaMark = prompt("Ingresa la masa Mark: ");
+let alturaMark = prompt("Ingresa la altura Mark: ");
+
+
+let masaJonh = prompt("Ingresa la masa de Jonh: ");
+let alturaJonh = prompt("Ingresa la altura de Jonh: ");  
+
+
+//  Calculo BMI
+  function calcularBMI(peso, altura) {
+    return peso / (altura * altura);
+  }
+  
+  // Calcular el IMC de Mark y John
+  let bmiMark = calcularBMI(masaMark, alturaMark);
+  let bmiJohn = calcularBMI(masaJonh, alturaJonh);
+  
+
+
+  if (bmiMark >bmiJohn){
+          console.log("el valor: " + markHigherBMI);
+  }else{
+    markHigherBMI = false;
+    console.log("el valor: "+markHigherBMI);
+  }
+  
+  // Mostrar los resultados
+  console.log("El IMC de Mark es: " + bmiMark);
+  console.log("El IMC de John es: " + bmiJohn);
+
