fff943f2735b77d09da36e649cd2b5edda6ee169
Juan Cisneros <juan.cisneros@grupoxalka.com>
2024-01-31 11:31:03 -0600

    Cambios actualizados del pull request

--- juanluis/rickandmorty/scripts.js
+++ juanluis/rickandmorty/scripts.js
@@ -1,24 +1,25 @@
 let url = "https://rickandmortyapi.com/api/";
-let html = document.querySelector(".mycontainer");
+let html = document.querySelector(".main-container");
 
 let arrayper = [];
 
-// Genera un array con 6 números aleatorios entre 1 y 100
+// Genera un array y toma los primeros 6 números, son aleatorios entre 1 y 826
+// que es el total de personajes existentes para que no me muestre más solo 6.
 for (let i = 0; i < 6; i++) {
   arrayper.push(Math.floor(Math.random() * (826 - 1) + 1));
 }
 
 // Función para obtener el nombre de un episodio a partir de su URL
-let obtenerEpisodioNombre = (episodeUrl) => {
+let getEpisodeName = (episodeUrl) => {
   return fetch(episodeUrl)
     .then((res) => res.json())
     .then((episodeData) => {
-      return episodeData.name;
+      return episodeData.name; //Aqui me recupera y me retorna el nombre del episodio
     });
 };
 
 // Función principal para obtener datos de personajes y episodios
-let obtenerDatos = (apiURL) => {
+let getDatos = (apiURL) => {
   return fetch(`${apiURL}character/${arrayper}`)
     .then((res) => res.json())
     .then(async (response) => {
@@ -26,21 +27,23 @@ let obtenerDatos = (apiURL) => {
       const tpt = await Promise.all(
         response.map(async (character) => {
           // Obtiene el nombre del episodio utilizando la función anterior
-          const episodeName = await obtenerEpisodioNombre(character.episode[0]);
+          const episodeName = await getEpisodeName(character.episode[0]);
           // Construye el HTML para cada personaje con su respectivo episodio
           return `<div class="sub-box">
                         <img src="${character.image}" id="img" alt="">
                         <div class="text-container">
-                            <div class="tituloper"><a href="#"><strong>${character.name}</strong></a></div>
-                            <div class="status">
-                                <div class="circle"></div>
-                                <p class="inftitulo">${character.status} - ${character.species}</p>
+                            <div class="name-character">
+                            <a href="#"><strong>${character.name}</strong></a>
                             </div>
-                            <p class="caracter1">Last known location:</p>
-                            <div class="infcaracter"><a href="#">${character.location.name}</a></div>
+                            <div class="status-character">
+                                <div class="circle-status"></div>
+                                <p class="data-status-species">${character.status} - ${character.species}</p>
+                            </div>
+                            <p class="location-character">Last known location:</p>
+                            <div class="nameLocation-nameEpisode"><a href="#">${character.location.name}</a></div>
 
-                            <p class="caracter2">First seen in:</p>
-                            <div class="infcaracter"><a href="#">${episodeName}</a></div>
+                            <p class="episode-character">First seen in:</p>
+                            <div class="nameLocation-nameEpisode"><a href="#">${episodeName}</a></div>
                         </div>
                     </div>`;
         })
@@ -51,4 +54,4 @@ let obtenerDatos = (apiURL) => {
 };
 
 // Llama a la función principal con la URL de la API
-obtenerDatos(url);
+getDatos(url);
