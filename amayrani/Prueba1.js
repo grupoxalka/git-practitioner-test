bdcb71e0bf36ad59547af6a072f272a38594a821
amayrani armas <amayrani.armas@grupoxalka.com>
2024-02-01 10:59:57 -0600

    modification in the code

--- amayrani/Prueba1.js
+++ amayrani/Prueba1.js
@@ -1,36 +1,62 @@
-//prueba1 BMI 
-//BMI = masa /altura /** 2 = masa / (altura * altura)
+// //prueba1 BMI
+// //BMI = masa /altura /** 2 = masa / (altura * altura)
 
-let markHigherBMI = true; 
+// let markHigherBMI = true;
 
-let masaMark = prompt("Ingresa la masa Mark: ");
-let alturaMark = prompt("Ingresa la altura Mark: ");
+// let masaMark = prompt("Ingresa la masa Mark: ");
+// let alturaMark = prompt("Ingresa la altura Mark: ");
 
+// let masaJonh = prompt("Ingresa la masa de Jonh: ");
+// let alturaJonh = prompt("Ingresa la altura de Jonh: ");
 
-let masaJonh = prompt("Ingresa la masa de Jonh: ");
-let alturaJonh = prompt("Ingresa la altura de Jonh: ");  
+// //  Calculo BMI
+//   function calcularBMI(peso, altura) {
+//     return peso / (altura * altura);
+//   }
 
+//   // Calcular el IMC de Mark y John
+//   let bmiMark = calcularBMI(masaMark, alturaMark);
+//   let bmiJohn = calcularBMI(masaJonh, alturaJonh);
 
-//  Calculo BMI
-  function calcularBMI(peso, altura) {
-    return peso / (altura * altura);
-  }
-  
-  // Calcular el IMC de Mark y John
-  let bmiMark = calcularBMI(masaMark, alturaMark);
-  let bmiJohn = calcularBMI(masaJonh, alturaJonh);
-  
+//   if (bmiMark >bmiJohn){
+//           console.log("el valor: " + markHigherBMI);
+//   }else{
+//     markHigherBMI = false;
+//     console.log("el valor: "+markHigherBMI);
+//   }
 
+//   // Mostrar los resultados
+//   console.log("El IMC de Mark es: " + bmiMark);
+//   console.log("El IMC de John es: " + bmiJohn);
 
-  if (bmiMark >bmiJohn){
-          console.log("el valor: " + markHigherBMI);
-  }else{
-    markHigherBMI = false;
-    console.log("el valor: "+markHigherBMI);
-  }
-  
-  // Mostrar los resultados
-  console.log("El IMC de Mark es: " + bmiMark);
-  console.log("El IMC de John es: " + bmiJohn);
+function greet(nombre, textOperacion, result, callback) {
+  let greetResult= `hola ${nombre} tu ${textOperacion} es ${result}`;
 
+  callback(greetResult);
+}
+                                      //f callback
+function userInput(num1, num2, nombre, callback) {
+  let sum = num1 + num2;
+  let operacion = "suma";
+  callback(num1, num2, nombre, greet);
+  //f callback                f callback
+}
+
+              //   15,   30,    juan ,   f callback
+function callback(num1, num2, nombre, callback) {
+  let resta = num1 - num2;
+  let operacion = "resta";
+  callback(nombre, operacion, resta,addGreet);
+ // f greet
+                          
+}
+
+function addGreet(result) {
+
+  console.log(result + "adios");
+
+}
+
+userInput(15, 30, "juan", callback);
+                         // f callback 
 
--- amayrani/rickandmorty/script.js
+++ amayrani/rickandmorty/script.js
@@ -2,13 +2,13 @@ const API_URL = "https://rickandmortyapi.com/api";
 //URL DEL API GENERAL
 
 //elementos de nuestro DOM
-const HTMLResponse = document.querySelector("#app");
-HTMLResponse.className = "app";
+const HTMLResponse = document.querySelector("#main-card-container");
+HTMLResponse.className = "main-card-container";
 //creacion del array que genera numeros aleatorios dentro 1 al 100
 let array = [];
 
 for (let i = 0; i < 6; i++) {
-  array.push(Math.floor(Math.random() * (100 - 1) + 1)); //genero numeros y convierte enteros
+  array.push(Math.floor(Math.random() * (100 - 1))); //genero numeros y convierte enteros
 }
 
 //funcnion que hace el llamado a la api  | el servicio fetch
@@ -18,9 +18,8 @@ fetch(`${API_URL}/character/${array}`)
     //parametro
 
     responseCharacters.forEach(async (responseCharacter) => {
-      //console.log(responseCharacter);
       //declaracion de propiedades
-      let documentFragment = document.createDocumentFragment(); // no es conteneodr es un fragmento para que se muestre en la pantalla 
+      let documentFragment = document.createDocumentFragment(); // no es conteneodr es un fragmento para que se muestre en la pantalla
       let divCard = document.createElement("div");
       let divImgContainer = document.createElement("div");
       let divInfoContainer = document.createElement("div");
@@ -30,7 +29,7 @@ fetch(`${API_URL}/character/${array}`)
       let divLocationContainer = document.createElement("div");
 
       let imgElement = document.createElement("img");
-      let aNameElement = document.createElement("a");
+      let aNameElement = document.createElement("h2");
       let pStatusElement = document.createElement("p");
       let pTitleLocationElement = document.createElement("p");
       let aLocationElement = document.createElement("a");
@@ -48,6 +47,7 @@ fetch(`${API_URL}/character/${array}`)
       aNameElement.className = "name-element";
       aEpisodeElement.className = "a-edit-container";
       aLocationElement.className = "a-edit-container";
+      imgElement.className = "imgCharacters";
 
       imgElement.src = `${responseCharacter.image}`;
       aNameElement.textContent = `${responseCharacter.name}`;
@@ -57,23 +57,14 @@ fetch(`${API_URL}/character/${array}`)
       pTitleEpisodeElement.textContent = `First seen in:`;
       aEpisodeElement.textContent = `${nameEpisode}`;
 
-      divImgContainer.appendChild(imgElement);
+      divImgContainer.append(imgElement);
+      divNameStatusContainer.append(aNameElement, pStatusElement);
+      divLocationContainer.append(pTitleLocationElement, aLocationElement);
+      divEpisodeContainer.append(pTitleEpisodeElement, aEpisodeElement);
+
+      divInfoContainer.append(divNameStatusContainer, divLocationContainer, divEpisodeContainer);
+      divCard.append(divImgContainer, divInfoContainer);
       documentFragment.appendChild(divCard);
-      divCard.appendChild(divImgContainer);
-
-      divNameStatusContainer.appendChild(aNameElement);
-      divNameStatusContainer.appendChild(pStatusElement);
-      divInfoContainer.appendChild(divNameStatusContainer);
-
-      divLocationContainer.appendChild(pTitleLocationElement);
-      divLocationContainer.appendChild(aLocationElement);
-      divInfoContainer.appendChild(divLocationContainer);
-
-      divEpisodeContainer.appendChild(pTitleEpisodeElement);
-      divEpisodeContainer.appendChild(aEpisodeElement);
-      divInfoContainer.appendChild(divEpisodeContainer);
-
-      divCard.appendChild(divInfoContainer);
 
       //Se muestra en el html
       HTMLResponse.appendChild(documentFragment); //le paso el contenerdor
@@ -81,13 +72,9 @@ fetch(`${API_URL}/character/${array}`)
   });
 
 async function getEpisode(url) {
-  //console.log(url);
-
   let respose = await fetch(`${url}`);
-  //console.log(respose);
 
   let data = await respose.json(); // lo convierte a json para que salieran los datos del episodio
-  //console.log(data);
 
   return data.name;
 }
