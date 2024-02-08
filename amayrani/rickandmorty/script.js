c7957a669b6862634b4308aacae36352cb93c627
amayraniarg <156934874+amayraniarg@users.noreply.github.com>
2024-02-08 10:30:28 -0600

    rickandmorty-Amayrani Armas (#2)
    * Modifico js de file amayran
    
    * Web page the rick and morty finish
    
    * Integration the rick and morty api
    
    * the rick and morty Api integration, modified
    
    * modification in the code
    
    * project update
    
    * footer changes

---
+++ amayrani/rickandmorty/script.js
@@ -0,0 +1,108 @@
+const API_URL = "https://rickandmortyapi.com/api";
+//URL DEL API GENERAL
+
+//elementos de nuestro DOM
+const HTMLResponse = document.querySelector("#main-card-container");
+HTMLResponse.className = "main-card-container";
+
+const divFooter = document.getElementById("word-container");
+divFooter.className = "word-container";
+
+//creacion del array que genera numeros aleatorios dentro 1 al 100
+let array = [];
+
+for (let i = 0; i < 6; i++) {
+  array.push(Math.floor(Math.random() * (826 - 1))); //genero numeros y convierte enteros
+}
+
+//funcnion que hace el llamado a la api  | el servicio fetch
+fetch(`${API_URL}/character/${array}`)
+  .then((response) => response.json()) //ya viene como json, lo convierte a un obj
+  .then((responseCharacters) => {
+    //parametro
+
+    responseCharacters.forEach(async (responseCharacter) => {
+      //declaracion de propiedades
+      let documentFragment = document.createDocumentFragment(); // no es conteneodr es un fragmento para que se muestre en la pantalla
+      let divCard = document.createElement("div");
+      let divImgContainer = document.createElement("div");
+      let divInfoContainer = document.createElement("div");
+
+      let divNameStatusContainer = document.createElement("div");
+      let divEpisodeContainer = document.createElement("div");
+      let divLocationContainer = document.createElement("div");
+
+      let imgElement = document.createElement("img");
+      let aNameElement = document.createElement("h2");
+      let pStatusElement = document.createElement("p");
+      let pTitleLocationElement = document.createElement("p");
+      let aLocationElement = document.createElement("a");
+      let pTitleEpisodeElement = document.createElement("p");
+      let nameEpisode = await getEpisode(responseCharacter.episode[0]);
+      let aEpisodeElement = document.createElement("a");
+
+      divCard.className = "card";
+      divImgContainer.className = "img-container";
+      divInfoContainer.className = "info-container";
+      divNameStatusContainer.className = "name-container";
+      aNameElement.className = "name-element";
+      divLocationContainer.className = "location-container";
+      divEpisodeContainer.className = "episode-container";
+      aNameElement.className = "name-element";
+      aEpisodeElement.className = "a-edit-container";
+      aLocationElement.className = "a-edit-container";
+      imgElement.className = "imgCharacters";
+      pStatusElement.className = "style-status";
+
+      imgElement.src = `${responseCharacter.image}`;
+      aNameElement.textContent = `${responseCharacter.name}`;
+      pStatusElement.textContent = `${responseCharacter.status} - ${responseCharacter.species}`;
+      pTitleLocationElement.textContent = `Last known location:`;
+      aLocationElement.textContent = `${responseCharacter.location.name}`; //acceso a una propiedad que contine un obj
+      pTitleEpisodeElement.textContent = `First seen in:`;
+      aEpisodeElement.textContent = `${nameEpisode}`;
+
+      divImgContainer.append(imgElement);
+      divNameStatusContainer.append(aNameElement, pStatusElement);
+      divLocationContainer.append(pTitleLocationElement, aLocationElement);
+      divEpisodeContainer.append(pTitleEpisodeElement, aEpisodeElement);
+
+      divInfoContainer.append(
+        divNameStatusContainer,
+        divLocationContainer,
+        divEpisodeContainer
+      );
+      divCard.append(divImgContainer, divInfoContainer);
+      documentFragment.appendChild(divCard);
+
+      //Se muestra en el html
+      HTMLResponse.appendChild(documentFragment); //le paso el contenerdor
+    });
+  });
+
+async function getEpisode(url) {
+  let respose = await fetch(`${url}`);
+
+  let data = await respose.json(); // lo convierte a json para que salieran los datos del episodio
+
+  return data.name;
+}
+
+//informacion footer
+let getFooterInfo = (apiInfo) => {
+  Promise.all([
+    fetch(`${apiInfo}/character`),
+    fetch(`${apiInfo}/location`),
+    fetch(`${apiInfo}/episode`),
+  ]).then(async ([characterInfo, locationInfo, episodeInfo]) => {
+    const characterFooter = await characterInfo.json();
+    const locationFooter = await locationInfo.json();
+    const episodeFooter = await episodeInfo.json();
+
+    divFooter.innerHTML = `<a class="a-footer" href="https://rickandmortyapi.com/api/character">CHARACTERS: ${characterFooter.info.count}</a>
+    <a class="a-footer" href="https://rickandmortyapi.com/api/location">LOCATIONS: ${locationFooter.info.count}</a>
+    <a class="a-footer" href="https://rickandmortyapi.com/api/episode">EPISODES: ${episodeFooter.info.count}</a>`;
+  });
+};
+
+getFooterInfo(API_URL);
